501a19c2bf4f48a66dd0ff909e3386a5c39e2399
Kate Murzina <murzinakate1@gmail.com>
2022-07-19 22:46:14 +0200

    add required user flow test
    - check emails for each comment for each user post
    - add method to check email
    - extract method to call API

--- src/test/java/com/kate/TestUserFlow.java
+++ src/test/java/com/kate/TestUserFlow.java
@@ -6,11 +6,14 @@ import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.path.json.JsonPath.from;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Epic("User flow API tests")
 @Feature("Validation for the emails in the user's post comments")
@@ -18,21 +21,60 @@ public class TestUserFlow {
 
     private static final String BASE_URL = "https://jsonplaceholder.typicode.com/";
     private static final String USERS_PATH = "users";
+    private static final String POSTS_PATH = "posts";
+    private static final String COMMENTS_PATH = "comments";
     private static final String USER_NAME_TO_FIND = "Delphine";
 
     @Test
-    @Story("User id could be found by username")
+    @Story("Emails in comments for user's posts are valid")
     @Severity(SeverityLevel.CRITICAL)
-    public void validateThereIsAUserWithCertainName() {
-        Response response = given()
+    public void testEmailsInUserPostsComments() {
+        Response response = requestByPath(USERS_PATH);
+
+        List<Integer> listOfUserIdsByUsername = from(response.asString()).getList(String.format("findAll {it.username == '%s'}.id", USER_NAME_TO_FIND));
+        assertEquals(1, listOfUserIdsByUsername.size(), "Expected user hasn't been found or we found more users with such name");
+
+        List<Integer> postIdsByUserId = getPostIdsByUserId(listOfUserIdsByUsername.get(0));
+        List<String> emailsByListOfPostIds = getEmailsByListOfPostIds(postIdsByUserId);
+        emailsByListOfPostIds.forEach(email -> assertTrue(checkEmailIsValid(email), String.format("Email: %s is not valid", email)));
+    }
+
+
+    @Step("Get posts by userId: {userId}")
+    private List<Integer> getPostIdsByUserId(Integer userId) {
+        Response response = requestByPath(POSTS_PATH);
+        return from(response.asString()).getList(String.format("findAll {it.userId == %s}.id", userId));
+    }
+
+    @Step("Get emails from comments by posts' ids")
+    private List<String> getEmailsByListOfPostIds(List<Integer> postIds) {
+        Response response = requestByPath(COMMENTS_PATH);
+        List<String> emails = new ArrayList<>();
+        postIds.forEach(postId -> emails.addAll(from(response.asString()).getList(String.format("findAll {it.postId == %s}.email", postId))));
+        return emails;
+    }
+
+    @Step("Request by path: {path}")
+    private Response requestByPath(String path) {
+        return given()
                 .filter(new AllureRestAssured())
                 .when()
-                .get(BASE_URL + USERS_PATH)
+                .get(BASE_URL + path)
                 .then()
                 .statusCode(HttpStatus.SC_OK)
                 .extract().response();
+    }
 
-        List<String> listOfUserIdsByUsername = from(response.asString()).getList(String.format("findAll {it.username == '%s'}.id", USER_NAME_TO_FIND));
-        assertEquals(1, listOfUserIdsByUsername.size(), "Expected user hasn't been found or we found more users with such name");
+    /**
+     * This is the simple validation which checks that email contains '@' symbol in the address
+     * @param email to check
+     * @return true if validation passed, otherwise returns false
+     */
+    @Step("Check that email is valid. Email: {email}")
+    private boolean checkEmailIsValid(String email) {
+        String regexPattern = "^(.+)@(\\S+)$";
+        return Pattern.compile(regexPattern)
+                .matcher(email)
+                .matches();
     }
 }
